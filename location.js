a49db500beaad4f03aa2ba82d6548a69269ca936
Dylan Pyle <me@dylanpyle.com>
2024-05-03 00:34:58 -0400

    Add source link

--- location.js
+++ location.js
@@ -10,7 +10,7 @@ async function run() {
 
 
   const tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>. <a href="https://github.com/dylanpyle/location">Source</a>',
     subdomains: 'abcd',
     maxZoom: 20
   });
